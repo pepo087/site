5d393d8967fd79b04733a8d8f43d4a4e5529c313
pompeo t. <48530804+pepo087@users.noreply.github.com>
2024-09-30 15:04:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ $(document).ready(function() {
     $('.skill-title').hover(
         function() {
             // Mostra il contenuto con slide down
-            $(this).next('.skill-content').stop(true, true).slideDown(300);
+            $(this).next('.skill-content').stop(true, true).slideDown(3000);
         },
         function() {
             // Nascondi il contenuto con slide up
-            $(this).next('.skill-content').stop(true, true).slideUp(300);
+            $(this).next('.skill-content').stop(true, true).slideUp(3000);
         }
     );
 });
