7e5842e107869266876179d565e723642e90d6ba
pompeo t. <48530804+pepo087@users.noreply.github.com>
2024-09-30 15:15:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ document.querySelectorAll('nav a').forEach(link => {
         
         // Nascondi tutte le sezioni
         document.querySelectorAll('section').forEach(sec => {
+            sec.querySelector('.section-content').classList.remove('visible'); // Rimuovi la classe visibile
             sec.style.display = "none"; // Nascondi tutte le sezioni
         });
 
         // Mostra la sezione cliccata
         section.style.display = "block"; // Mostra la sezione selezionata
- // Aggiungi un timeout per assicurarti che la sezione sia visibile prima di aggiungere la classe
+
+        // Aggiungi un timeout per assicurarti che la sezione sia visibile prima di aggiungere la classe
         setTimeout(() => {
             section.querySelector('.section-content').classList.add('visible'); // Aggiungi la classe visibile
         }, 50); // Breve ritardo per permettere alla sezione di essere visibile
+
         event.preventDefault(); // Impedisce il comportamento predefinito del link
     });
 });
+
 
 // Mostra/nascondi il contenuto delle competenze al passaggio del mouse
 $(document).ready(function() {
