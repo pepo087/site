496e73d670b3b8c6f310ec216bdaf6f10098e1d6
pompeo t. <48530804+pepo087@users.noreply.github.com>
2024-10-03 16:47:58 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,8 +1,11 @@
-// Inizializza le particelle con la configurazione dal file JSON
-particlesJS.load('particles-js-header', 'Particles.json', function() {
-    console.log('callback - particles.js config loaded');
+
+document.addEventListener("DOMContentLoaded", function() {
+    particlesJS.load('particles-js-header', 'Particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
 });
-// Inizializza le particelle con la configurazione dal file JSON
-particlesJS.load('particles-js-footer', 'Particles.json', function() {
-    console.log('callback - particles.js config loaded');
+document.addEventListener("DOMContentLoaded", function() {
+    particlesJS.load('particles-js-footer', 'Particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
 });
