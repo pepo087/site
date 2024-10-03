f2d7790ccedc64dd38ece074bb2a2b266a7ec821
pompeo t. <48530804+pepo087@users.noreply.github.com>
2024-10-03 16:54:18 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,9 +1,1 @@
 
-    particlesJS.load('particles-js-header', 'Particles.json', function() {
-        console.log('callback - particles.js config loaded');
-    });
-
-    particlesJS.load('particles-js-footer', 'Particles.json', function() {
-        console.log('callback - particles.js config loaded');
-    });
-
