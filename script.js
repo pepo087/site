bf72088994c3e8059abb678f3dea4e7300dff55d
pompeo t. <pepo087@gmail.com>
2024-10-03 11:14:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,6 +40,15 @@ document.querySelectorAll('nav a').forEach(link => {
     });
 });
 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    particlesJS.load('particles-js', 'path/to/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+});
+
+
     function toggleFolder() {
         var folderContainer = document.getElementById('folderContainer');
         folderContainer.style.display = folderContainer.style.display === 'none' ? 'block' : 'none';
