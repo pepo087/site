0bd1ec4952e79edccf9876ab8e09088095e303fa
pompeo t <pepo087@gmail.com>
2024-10-03 12:21:31 +0200

    particle.json

---
+++ particle.js
@@ -0,0 +1,4 @@
+// Inizializza le particelle con la configurazione dal file JSON
+particlesJS.load('particles-js', 'Particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
